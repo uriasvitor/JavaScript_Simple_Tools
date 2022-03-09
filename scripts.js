bc5e80e792f005f084c4830590fa245c60d66012
SAIRU <uriasvitor@gmail.com>
2022-03-09 01:55:27 -0300

    StyleChange

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 
 
+document.getElementById('area').addEventListener("input",ShowLength);
+
 function ShowLength(){
-    let turn = document.getElementById('Length');
+    var turn = document.getElementById('Length');
 
     if(turn.checked){
         let textArea = document.getElementById('area').value;
@@ -10,6 +12,27 @@ function ShowLength(){
         document.getElementById('results').innerHTML = length;
         
     }else{
+        document.getElementById('results').innerHTML = '';
         console.log(turn.value)
     }
+}
+
+function ShowColor(){
+     let turn = document.getElementById('Color');
+    
+    if(turn.checked){
+        let textArea = document.getElementById('area').value;
+        let length = textArea.length;
+        alert("asodoas")
+
+        document.getElementById('results').innerHTML = length;
+        
+    }else{
+        document.getElementById('results').innerHTML = '';
+        console.log(turn.value)
+    }
+
+    let color = document.getElementById('area').value;
+    
+
 }
